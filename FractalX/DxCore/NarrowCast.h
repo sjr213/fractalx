cc95a1832bb474763d5106a0dfc5264dd24894ba
Steve Robles <sjrobles@yahoo.com>
2021-06-05 21:04:31 -0400

    Implemented narrow and narrow_cast.

--- FractalX/DxCore/NarrowCast.h
+++ FractalX/DxCore/NarrowCast.h
@@ -3,17 +3,15 @@
 #include <typeinfo>
 #include <sstream> 
 
-// adapted from
-// https://codereview.stackexchange.com/questions/231364/implementation-of-narrow-cast-in-c
 namespace DxCore
 {
-	class bad_narrow_cast : public std::bad_cast
+	class narrowing_error : public std::bad_cast
 	{
 	public:
-		bad_narrow_cast()
+		narrowing_error() : what_("cast failed")
 		{}
 
-		bad_narrow_cast(char const* message) : what_(message)
+		narrowing_error(char const* message) : what_(message)
 		{}
 
 		char const* what() { return what_; }
@@ -22,6 +20,10 @@ namespace DxCore
 		char const* what_;
 	};
 
+	
+	// adapted from
+	// https://codereview.stackexchange.com/questions/231364/implementation-of-narrow-cast-in-c
+	/*
 	template <typename ToType, typename FromType> static inline
 		ToType narrow_cast(FromType const& from)
 	{
@@ -36,6 +38,33 @@ namespace DxCore
 		ss << typeid(FromType).name();
 		ss << " to type ";
 		ss << typeid(ToType).name();
-		throw bad_narrow_cast(ss.str().c_str());
+		throw narrowing_error(ss.str().c_str());
+	}
+	*/
+
+	template <typename T, typename U>
+	bool AreSameSign()
+	{
+		return (std::is_signed<T>::value == std::is_signed<U>::value);
+	}
+
+	// narrow_cast(): a searchable way to do narrowing casts of values
+	template <typename T, typename U>
+	constexpr T narrow_cast(U&& u) noexcept
+	{
+		return static_cast<T>(std::forward<U>(u));
+	}
+
+	// narrow() : a checked version of narrow_cast() that throws if the cast changed the value
+	// Don't use for double -> float
+	template <typename T, typename U>
+	T narrow(U u) noexcept(false)
+	{
+		T t = narrow_cast<T>(u);
+		if (static_cast<U>(t) != u)
+			throw narrowing_error();
+		if (!AreSameSign<T,U>() && ((t < T{}) != (u < U{})))  // <-- ???
+			throw narrowing_error();
+		return t;
 	}
 }
--- FractalX/DxTests/NarrowCastTest.cpp
+++ FractalX/DxTests/NarrowCastTest.cpp
@@ -12,84 +12,104 @@ namespace DxTests
 	TEST_CLASS(narrowCastTest)
 	{
 	public:
-		TEST_METHOD(TestIntToBite)
+		TEST_METHOD(TestAreSameSignInts)
+		{
+			bool result = AreSameSign<int, int>();
+			Assert::IsTrue(result, L"int and int should be the same sign", LINE_INFO());
+		}
+
+		TEST_METHOD(TestAreSameSignIntUnsignedInt)
+		{
+			bool result = AreSameSign<int, unsigned int>();
+			Assert::IsFalse(result, L"int and unsigned int should not be the same sign", LINE_INFO());
+		}
+
+		TEST_METHOD(TestAreSameSignIntInt8)
+		{
+			bool result = AreSameSign<int, __int8>();
+			Assert::IsTrue(result, L"int and __int8 should be the same sign", LINE_INFO());
+		}
+
+		TEST_METHOD(TestAreSameSignIntUInt8)
+		{
+			bool result = AreSameSign<int, unsigned __int8>();
+			Assert::IsFalse(result, L"int and unsigned __int8 should not be the same sign", LINE_INFO());
+		}
+
+		TEST_METHOD(TestAreSameSignDoubleFloat)
+		{
+			bool result = AreSameSign<double, float>();
+			Assert::IsTrue(result, L"double and float should be the same sign", LINE_INFO());
+		}
+
+		TEST_METHOD(TestNarrowIntToBite)
 		{
 			try
 			{
 				int x = 10;
-				unsigned __int8 result = narrow_cast<unsigned __int8>(x);
-				Assert::AreEqual(result, 10ui8, L"Narrow cast 10 to bite failed", LINE_INFO());
+				unsigned __int8 result = narrow<unsigned __int8>(x);
+
+				Assert::AreEqual(result, 10ui8, L"Narrow 10 to bite failed", LINE_INFO());
 			}
-			catch (bad_narrow_cast&)
+			catch (narrowing_error&)
 			{
-				Assert::Fail(L"Narrow cast 10 to bite exception", LINE_INFO());
+				Assert::Fail(L"Narrow int 10 to __int8 threw", LINE_INFO());
 			}
 		}
 
-		TEST_METHOD(TestIntToBiteShouldFail)
+		TEST_METHOD(TestNarrowIntToBiteShouldFail)
 		{
 			try
 			{
 				int x = 300;
-				unsigned __int8 result = narrow_cast<unsigned __int8>(x);
-				Assert::Fail(L"Narrow cast 300 to bite should have failed", LINE_INFO());
+				unsigned __int8 result = narrow<unsigned __int8>(x);
+				Assert::Fail(L"Narrow int 300 to __int8 did not throw?", LINE_INFO());
 			}
-			catch (bad_narrow_cast&)
+			catch (narrowing_error&)
 			{
+				// expect it to throw
 			}
 		}
 
-		TEST_METHOD(TestIntToUInt)
+		TEST_METHOD(TestNarrowIntToUInt)
 		{
 			try
 			{
 				int x = 500;
-				unsigned int result = narrow_cast<unsigned int>(x);
-				Assert::AreEqual(result, 500u, L"Narrow cast 500 to uint failed", LINE_INFO());
+				unsigned int result = narrow<unsigned int>(x);
+				Assert::AreEqual(500u, result, L"Narrow 500 to uint should not fail", LINE_INFO());
 			}
-			catch (bad_narrow_cast&)
+			catch (narrowing_error&)
 			{
-				Assert::Fail(L"Narrow cast 500 to uint exception", LINE_INFO());
+				Assert::Fail(L"Narrow 500 to uint should not throw", LINE_INFO());
 			}
 		}
 
-		TEST_METHOD(TestNegativeIntToUInt)
+		TEST_METHOD(TestNarrowNegativeIntToUInt)
 		{
 			try
 			{
 				int x = -500;
-				unsigned int result = narrow_cast<unsigned int>(x);
-				Assert::Fail(L"Narrow cast -500 to unit should have failed", LINE_INFO());
+				unsigned int result = narrow<unsigned int>(x);
+				Assert::Fail(L"Narrow -500 to uint should throw", LINE_INFO());
 			}
-			catch (bad_narrow_cast&)
+			catch (narrowing_error&)
 			{
+
 			}
 		}
 
-		TEST_METHOD(TestDoubleToFloat)
+		TEST_METHOD(TestNarrowCastDoubleToFloat)
 		{
 			try
 			{
 				double x = 3.14;
 				float result = narrow_cast<float>(x);
-				Assert::AreEqual(result, 3.14f, 0.00001f, L"Narrow cast 500 to uint failed", LINE_INFO());
+				Assert::AreEqual(result, 3.14f, 0.00001f, L"Narrow cast 3.14 to float should have passed", LINE_INFO());
 			}
-			catch (bad_narrow_cast&)
+			catch (narrowing_error&)
 			{
-				Assert::Fail(L"Narrow cast 3.14 to float threw exception", LINE_INFO());
-			}
-		}
-
-		TEST_METHOD(TestDoubleToBigToFloat)
-		{
-			try
-			{
-				double x = 1.2e100;
-				float result = narrow_cast<float>(x);
-				Assert::Fail(L"Narrow cast 1.2e100 to float should have failed", LINE_INFO());
-			}
-			catch (bad_narrow_cast&)
-			{
+				Assert::Fail(L"Narrow 3.14 to float should not throw", LINE_INFO());
 			}
 		}
 
